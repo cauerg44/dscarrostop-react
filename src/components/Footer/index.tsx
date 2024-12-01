74e40363039cc436e87e8d8c2ae9b8e23245e6f6
Cauê Garcia <cauerg7@outlook.com>
2024-12-01 08:39:30 -0300

    Refactoring Footer.

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,14 +4,7 @@ export default function Footer() {
 
     return (
         <footer>
-            <div className='ct-footer-element'>
-                <div>
-                    <a href="#">@dscarrostop</a>
-                </div>
-                <div>
-                    <address>Rua da Pipoca, 150</address>
-                </div>
-            </div>
+            
         </footer>
     )
 }
