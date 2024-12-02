7ae69921dc723f0f929e340685e883b539a48357
Cauê Garcia <cauerg7@outlook.com>
2024-12-02 10:18:21 -0300

    Footer styles on 1200px done

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,8 +3,9 @@ import './styles.css'
 export default function Footer() {
 
     return (
-        <footer>
-            
+        <footer className="ct-comments-container ct-footer-elements">
+            <a href="#">@dscarrostop</a>
+            <address>Rua da Pipoca, 150</address>
         </footer>
     )
 }
